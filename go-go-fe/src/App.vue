7ae47a4592b67b623f65fad3659e22a7cf32ccd1
lmhuong711 <lmhuong711@gmail.com>
2024-05-25 17:40:33 +0700

    init fe

---
+++ go-go-fe/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <vdt-list />
+</template>
+
+<script>
+import VdtList from "./components/VdtList.vue";
+
+export default {
+  name: "App",
+  setup() {
+    return {};
+  },
+  components: {
+    VdtList,
+  },
+};
+</script>
+
+<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.padding {
+  padding: 20px;
+}
+</style>
---
+++ go-go-fe/src/utils/constans.js
@@ -0,0 +1,3 @@
+const API_URL = process.env.VUE_APP_BASE_URL;
+
+export { API_URL };
